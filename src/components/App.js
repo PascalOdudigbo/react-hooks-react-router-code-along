743b02fa3993edd8f5ff2c127b2303792ed17acb
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-07-22 01:49:56 +0300

    Done.

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+// src/components/App.js
+import React from "react";
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+        <NavBar/>
+        <Routes>
+        <Route exact path="/" element={ <Home />}/>
+        
+        <Route path="/about" element={ <About />}/>
+        
+        <Route path="/login"  element={ <Login />}/>
+        
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
